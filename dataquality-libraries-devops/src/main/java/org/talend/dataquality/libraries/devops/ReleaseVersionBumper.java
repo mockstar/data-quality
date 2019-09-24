746aca8675a7eac75453a733dc174e0efbbb9b51
build-talend <build-talend@users.noreply.github.com>
2019-09-24 16:49:05 +0200

    chore(Jenkins): Bump to next dev version 7.3.0-SNAPSHOT (#715)
    * chore(Jenkins): Bump to next dev version 7.3.0-SNAPSHOT
    
    * update DQ EE lib version for components

--- dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
+++ dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
@@ -58,7 +58,7 @@ import org.xml.sax.SAXException;
  */
 public class ReleaseVersionBumper {
 
-    private static final String TARGET_VERSION = "7.2.2-SNAPSHOT";
+    private static final String TARGET_VERSION = "7.3.0-SNAPSHOT";
 
     private static final String TARGET_DAIKON_VERSION = "1.6.0";
 
