45514e8bfa726450da2aa844d81128d690f1aa42
build-talend <build-talend@users.noreply.github.com>
2019-10-21 14:27:24 +0200

    chore(Jenkins): Release DQ libraries 7.3.0 (#721)

--- dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
+++ dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
@@ -56,9 +56,9 @@ import org.xml.sax.SAXException;
  */
 public class ReleaseVersionBumper {
 
-    private static final String TARGET_VERSION = "7.3.0-SNAPSHOT";
+    private static final String TARGET_VERSION = "7.3.0";
 
-    private static final String TARGET_DAIKON_VERSION = "1.6.0";
+    private static final String TARGET_DAIKON_VERSION = "1.7.0";
 
     private static final String DAIKON_VERSION_PROPERTY_NAME = "org.talend.daikon.version";
 
